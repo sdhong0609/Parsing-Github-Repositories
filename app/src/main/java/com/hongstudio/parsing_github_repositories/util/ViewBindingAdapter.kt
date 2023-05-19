184227c740bc55df2c3969ea4812e547390133f8
hong <sdhong0609@gmail.com>
2023-05-20 01:31:25 +0900

    fix: 안드로이드 스튜디오 버전과 gradle 및 jdk 버전에 맞게 수정

--- app/src/main/java/com/hongstudio/parsing_github_repositories/util/ViewBindingAdapter.kt
+++ app/src/main/java/com/hongstudio/parsing_github_repositories/util/ViewBindingAdapter.kt
@@ -4,7 +4,7 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
-@BindingAdapter("app:imageUrl")
+@BindingAdapter("imageUrl")
 fun imageUrl(imageView: ImageView, url: String) {
     Glide.with(imageView.context).load(url).into(imageView)
 }
