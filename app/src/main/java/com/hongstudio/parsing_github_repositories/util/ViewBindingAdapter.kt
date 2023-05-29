016fc960a59e98932c79c099df4fe6a3b4139ff9
hong <sdhong0609@gmail.com>
2023-05-29 20:29:41 +0900

    fix: Glide 로딩중 에러 처리

--- app/src/main/java/com/hongstudio/parsing_github_repositories/util/ViewBindingAdapter.kt
+++ app/src/main/java/com/hongstudio/parsing_github_repositories/util/ViewBindingAdapter.kt
@@ -4,10 +4,11 @@ import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.hongstudio.parsing_github_repositories.R
 
 @BindingAdapter("imageUrl")
 fun ImageView.imageUrl(url: String?) {
-    Glide.with(this).load(url).into(this)
+    Glide.with(this).load(url).error(R.drawable.no_data).into(this)
 }
 
 @BindingAdapter("isVisible")
